b1a02d1406f9dc065ea2538ebeb58a6a3dfb2992
miwa_maroon <65750938+miwa-maroon@users.noreply.github.com>
2022-09-28 17:47:20 +0900

    update

---
+++ Source/RxSample/PuyoPuyo/ScoreWidget.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ScoreWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RXSAMPLE_API UScoreWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+	void SetScoreText(int32 Score);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void SetGameOverText();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void HideGameOverText();
+	
+};
